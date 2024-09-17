d89b78b7231652c2d0b5a4279513952d23db4a7b
TCFig <tifigo@LAPTOP-TIAGOFIG>
2024-09-17 18:02:38 +0100

    made some changes to Ch5_exercises.py

--- Ch5_exercises.py
+++ Ch5_exercises.py
@@ -1,3 +1,7 @@
+# Exercise 1
+print(f'\nExercise 1\n')
+
+
 def divide_by_elems(filename,x):
     quotients = []
     try:
@@ -6,27 +10,6 @@ def divide_by_elems(filename,x):
                 try:
                     frac = x / int(n)
                     quotients.append(frac)
-                except ZeroDivisionError:
-                    print("divide_by_elems: Division by zero.")
-                except ValueError:
-                    print("divide_by_elems: Character could not be converted to int.")
-    except IOError:
-        print("divide_by_elems: A file-related problem occurred.")
-
     return quotients
 
     
-data = divide_by_elems('numbers.txt', 2)
-print(data)
-
-def compute_ratios(xs,ys):
-    ratios = []
-    for i in range(min(len(xs),len(ys))):
-        try:
-            ratios.append(xs[i]/ys[i])
-        except ZeroDivisionError:
-            ratios.append(float('NaN')) # NaN = Not a Number
-    return ratios
-
-print(compute_ratios([2,3,4],[2,0,5]))
-a = compute_ratios([2,3,4,0],[2,0,5])
